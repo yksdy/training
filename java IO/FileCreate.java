bc13994269c4fc8afc8bafa959abdc30d361d5ae
yksdy <yksdym@163.com>
2014-07-10 14:25:22 +0800

    javIO File/
    delete()  isDirectory() list() listFiles() mkdir() length File.separator
    createNewFile() File

--- java IO/FileCreate.java
+++ java IO/FileCreate.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 public class FileCreate {
 	public static void main(String[] args){
 		//File file = new File("C:\\Users\\sky042449\\Documents\\GitHu\\training\\java IO\\test.txt"); //Error
-		//File file = new File("testt.txt");  //ok
-		File file = new File("F:\\test.txt");  //ok
+		File file = new File("testt.txt");  //ok
+		//File file = new File("F:\\test.txt");  //ok
 		//File file = new File("\\a\\testt.txt"); //Error
 		try{
 			file.createNewFile();
