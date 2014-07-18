01ab031c1d59464745c01956fd6737e2bfdfe610
yksdy <yksdym@163.com>
2014-07-18 20:47:02 +0800

    JavaIO review

--- java 网络编程/ServerDemo.java
+++ java 网络编程/ServerDemo.java
@@ -6,6 +6,13 @@ public class ServerDemo {
 		Socket client = server.accept();
 		PrintStream out = new PrintStream(client.getOutputStream());
 		out.println("I'm server , are you client? what art you doing?");
+		BufferedReader buf = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		for (int i =0; i<6; i++){
+			if(buf.readLine()!="\n"){
+				System.out.println("echo: "+ buf.readLine());
+			}
+		}
+		buf.close();
 		client.close();
 		server.close();
 	}
